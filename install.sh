e5cf1ccd2d473c465eb44258c6eda2ab45e133b3
ney <ney@users.noreply.github.com>
2026-04-11 23:33:06 +0300

    Update install.sh for 1-click execution

--- install.sh
+++ install.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-# Ney Firmware Installer
+# Ney Firmware Support Installer
 
 if [ "$(id -u)" -ne 0 ]; then
-  echo "Please run as root (sudo ./install.sh)"
+  echo "Please run as root"
   exit 1
 fi
+
+cd "$(dirname "$0")"
 
 echo "Installing Linux Ney Firmware..."
 cp -r usr/bin/* /usr/bin/
 chmod +x /usr/bin/*ney*
+chmod +x /usr/bin/infoney
 
 echo "Installation complete!"
-echo "You can now run commands like: wifiney, ssney, wgney, infoney"
+echo "Commands available: wifiney, ssney, wgney, infoney, ipney, resetney"
