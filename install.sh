decb08603900dd6b05146e7b5ac9f37ea780d80f
ney <ney@users.noreply.github.com>
2026-04-11 23:42:53 +0300

    Add bash aliases

--- install.sh
+++ install.sh
@@ -13,5 +13,15 @@ cp -r usr/bin/* /usr/bin/
 chmod +x /usr/bin/*ney*
 chmod +x /usr/bin/infoney
 
+# Setting up global aliases
+cat << 'ALIAS_EOF' > /etc/profile.d/ney_aliases.sh
+alias i='infoney'
+alias w='wifiney'
+alias s='ssney'
+alias reset='resetney'
+ALIAS_EOF
+chmod +x /etc/profile.d/ney_aliases.sh
+
 echo "Installation complete!"
 echo "Commands available: wifiney, ssney, wgney, infoney, ipney, resetney"
+echo "Shortcuts: 'i' (Info/Menu), 'w' (WiFi), 's' (Shadowsocks), 'reset' (Kill-Switch)"
