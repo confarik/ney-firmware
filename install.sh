b6be22c86695c909a373d140c25a04b500bc2f78
ney <ney@users.noreply.github.com>
2026-04-11 23:53:35 +0300

    Fix interactive menu alias and install OS dependencies

--- install.sh
+++ install.sh
@@ -8,14 +8,22 @@ fi
 
 cd "$(dirname "$0")"
 
-echo "Installing Linux Ney Firmware..."
+echo "Installing Linux Ney Firmware Dependencies..."
+apt update
+apt install -y fzf NetworkManager openvpn wireguard iptables vnstat resolvconf sudo curl wget jq
+
+echo "Creating dummy structure for clean OS..."
+mkdir -p /etc/NetworkManager/system-connections/
+mkdir -p /etc/default
+touch /etc/default/openvpn
+
+echo "Installing Linux Ney Firmware Scripts..."
 cp -r usr/bin/* /usr/bin/
 chmod +x /usr/bin/*ney*
-chmod +x /usr/bin/infoney
 
 # Setting up global aliases
 cat << 'ALIAS_EOF' > /etc/profile.d/ney_aliases.sh
-alias i='infoney'
+alias i='iney'
 alias w='wifiney'
 alias s='ssney'
 alias reset='resetney'
@@ -23,5 +31,5 @@ ALIAS_EOF
 chmod +x /etc/profile.d/ney_aliases.sh
 
 echo "Installation complete!"
-echo "Commands available: wifiney, ssney, wgney, infoney, ipney, resetney"
-echo "Shortcuts: 'i' (Info/Menu), 'w' (WiFi), 's' (Shadowsocks), 'reset' (Kill-Switch)"
+echo "Commands available: wifiney, ssney, wgney, iney, ipney, resetney"
+echo "Shortcuts: 'i' (Interactive Menu), 'w' (WiFi), 's' (Shadowsocks), 'reset' (Kill-Switch)"
